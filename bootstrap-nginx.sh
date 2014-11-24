5a4c39325b2f1fefd0970784246a030d605acfd6
Trevor Muñoz <trevor.munoz@gmail.com>
2014-11-24 09:56:43 -0500

    varible takes name from repo

--- bootstrap-nginx.sh
+++ bootstrap-nginx.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-/bin/sed -i "s/<es_server_placeholder>/${DOCKERKAPSITE_ES_1_PORT_9200_TCP_ADDR}/" /etc/nginx/sites-available/search
+/bin/sed -i "s/<es_server_placeholder>/${DOCKERNGINXES_ES_1_PORT_9200_TCP_ADDR}/" /etc/nginx/sites-available/search
 /etc/init.d/nginx start
 exec "$@"
