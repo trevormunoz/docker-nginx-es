5beeb17f21f5580ce1c9db3212e7b7c4938d3f0d
Trevor Muñoz <trevor.munoz@gmail.com>
2015-01-14 10:20:59 -0500

    try different exec pattern in shell script

--- dumbwaiter/dw.sh
+++ dumbwaiter/dw.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-/usr/local/bin/anaconda/envs/dumbwaiter/bin/python -m dumbwaiter \
+exec -c "/usr/local/bin/anaconda/envs/dumbwaiter/bin/python -m dumbwaiter \
 /var/lib/dumbwaiter/data \
 -s ${DOCKERNGINXES_ES_1_PORT_9200_TCP_ADDR} \
--p 9200
-exec "$@"
+-p 9200"
