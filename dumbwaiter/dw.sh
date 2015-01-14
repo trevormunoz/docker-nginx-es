0090b19e8895b891c6df1e2f7c1fbb73364d6351
Trevor Muñoz <trevor.munoz@gmail.com>
2015-01-13 22:26:31 -0500

    Run dumbwaiter via python -m

--- dumbwaiter/dw.sh
+++ dumbwaiter/dw.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-dumbwaiter /var/lib/dumbwaiter/data/2014_12_01_07_01_16_data.tgz -s ${DOCKERNGINXES_ES_1_PORT_9200_TCP_ADDR} -p 9200
+/usr/local/bin/anaconda/envs/dumbwaiter/bin/python -m dumbwaiter \
+/var/lib/dumbwaiter/data/2014_12_01_07_01_16_data.tgz \
+-s ${DOCKERNGINXES_ES_1_PORT_9200_TCP_ADDR} \
+-p 9200
 exec "$@"
