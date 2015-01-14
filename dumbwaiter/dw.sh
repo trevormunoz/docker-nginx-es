096c08f0e541ce575d8f0d03639304829c5d5d76
Trevor Muñoz <trevor.munoz@gmail.com>
2015-01-14 16:25:40 -0500

    tail log file in script

--- dumbwaiter/dw.sh
+++ dumbwaiter/dw.sh
@@ -2,4 +2,5 @@
 exec /usr/local/bin/anaconda/envs/dumbwaiter/bin/python -m \
 dumbwaiter /var/lib/dumbwaiter/data \
 -s ${DOCKERNGINXES_ES_1_PORT_9200_TCP_ADDR} \
--p ${DOCKERNGINXES_ES_1_PORT_9200_TCP_PORT}
+-p ${DOCKERNGINXES_ES_1_PORT_9200_TCP_PORT} &&\
+&& tail -F /var/log/dumbwaiter/nypl_menus_data_transform.log
