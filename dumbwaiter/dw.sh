24154e94dd91e9d9c849e69b9e3f604a82705f1d
Trevor Muñoz <trevor.munoz@gmail.com>
2015-01-14 10:28:09 -0500

    More exec fumbling

--- dumbwaiter/dw.sh
+++ dumbwaiter/dw.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-exec -c "/usr/local/bin/anaconda/envs/dumbwaiter/bin/python -m dumbwaiter \
-/var/lib/dumbwaiter/data \
+exec /usr/local/bin/anaconda/envs/dumbwaiter/bin/python -c \
+"-m dumbwaiter /var/lib/dumbwaiter/data \
 -s ${DOCKERNGINXES_ES_1_PORT_9200_TCP_ADDR} \
 -p 9200" "$@"
