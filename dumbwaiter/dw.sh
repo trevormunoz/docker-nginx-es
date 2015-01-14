e5daf2cc3875c9bfc835c2d10aabe5c278b2baf6
Trevor Muñoz <trevor.munoz@gmail.com>
2015-01-14 16:46:06 -0500

    Tailing log didn't keepalive

--- dumbwaiter/dw.sh
+++ dumbwaiter/dw.sh
@@ -3,4 +3,4 @@ exec /usr/local/bin/anaconda/envs/dumbwaiter/bin/python -m \
 dumbwaiter /var/lib/dumbwaiter/data \
 -s ${DOCKERNGINXES_ES_1_PORT_9200_TCP_ADDR} \
 -p ${DOCKERNGINXES_ES_1_PORT_9200_TCP_PORT}
-exec tail -F /var/log/dumbwaiter/nypl_menus_data_transform.log
+exec "$@"
